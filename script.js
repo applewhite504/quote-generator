6d4f1afb72b387858c3cab5693f0997da65edfaf
Beth Applewhite <bethapplewhite.nola@gmail.com>
2020-09-04 09:51:06 -0500

    took out loader

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
-const loader = document.getElementById('loader');
+// const loader = document.getElementById('loader');
 
 // Show loading
-function loading() {
-    loader.hidden = false;
-    quoteContainer.hidden = true;
-}
+// function loading() {
+//     loader.hidden = false;
+//     quoteContainer.hidden = true;
+// }
 
-// Hide Loading
-function complete() {
-    if (!loader.hidden) {
-        quoteContainer.hidden = false;
-        loader.hidden = true;
-    }
-}
+// // Hide Loading
+// function complete() {
+//     if (!loader.hidden) {
+//         quoteContainer.hidden = false;
+//         loader.hidden = true;
+//     }
+// }
 
 
 
